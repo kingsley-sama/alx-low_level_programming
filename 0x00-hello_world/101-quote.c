a4926130094d31b4d3f7698012c611393972d56a
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-05 23:18:57 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 /**
- * main - print a message
+ * main - print a message to stderr
  * Return: 0
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+    write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 0);
+    return (1);
 }
