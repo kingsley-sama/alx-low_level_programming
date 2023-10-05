ea9d5d25cec3a8b9eaea598e29ff168cfe79587f
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-05 23:11:31 +0100

    fixes bug in 100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 /**
  * main - print a message
  * Return: 0
@@ -11,6 +12,6 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-	
+	write(STDERR_FILENO, "[anything]", 0);
 	return (0);
 }
