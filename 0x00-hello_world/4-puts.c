7321e1ecca4dbe8e3420a03241948486a34caccd
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-05 15:20:21 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - print a message
+ * Return: 0
+ */
+int main(void)
 {
-        puts("Programming is like building a multilingual puzzle");
-        return(0);
+	puts("hello");
+	return (0);
 }
