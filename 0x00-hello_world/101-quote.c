a62f1eec7963a93439e0010e152f38588014abd4
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-05 22:28:23 +0100

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print a message
+ * Return: 0
+ */
+int main(void)
+{
+	char error[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, error);
+	return (0);
+}
