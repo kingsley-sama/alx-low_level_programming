d8de3aeb50bfd57bb062bc12c50f6569052b70be
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-06 17:49:34 +0100

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - print a message
+ * main - Print lowercase alphabets followed by uppercase
+ *alphabets and a new line
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	int a = 97;
+
+	for (; a < 123; a++)
+	{
+		if (a == 101 || a == 113)
+			;
+		else
+			putchar(a);
+
+
+	}
+
+	putchar('\n');
 	return (0);
 }
