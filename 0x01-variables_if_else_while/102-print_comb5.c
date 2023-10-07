fce1782914ea3b27fe6cfba7933a877144440abf
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-07 01:52:07 +0100

    this days i rarely rest 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
+/**
+ * print_pair - a unique pair of two-digit numbers in ascending order.
+ * @a: The first digit of the pair.
+ * @b: The second digit of the pair.
+ * @c: The third digit of the pair.
+ * @d: The fourth digit of the pair.
+ */
 
+void print_pair(char a, char b, char c, char d)
+{
+	putchar(a);
+	putchar(b);
+	putchar(' ');
+	putchar(c);
+	putchar(d);
+
+	if (a != '9' || b != '8' || c != '9' || d != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+
+/**
+ * main - Print unique pairs of two-digit numbers in ascending order
+ * Return: 0
+ */
 int main(void)
 {
-	int a, b, c, d;
+	char a, b, c, d;
 
-	for (a = 0; a <= 9; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = a; b <= '9'; b++)
 		{
-			for (c = 0; c <= 9; c++)
+			for (c = '0'; c <= '9'; c++)
 			{
-				for (d = 0; d <= 9; d++)
-				{
-					if (a * 10 + b <= c * 10 + d)
-					{
-						putchar(a + '0');
-						putchar(b + '0');
-						putchar(' ');
-						putchar(c + '0');
-						putchar(d + '0');
+				for (d = (a == b) ? (b + 1) : '0'; d <= '9'; d++)
+					print_pair(a, b, c, d);
 
-						if (!(a == 9 && b == 9 && c == 9 && d == 9))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
 			}
 		}
 	}
