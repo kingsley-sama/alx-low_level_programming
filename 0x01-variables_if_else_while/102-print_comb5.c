f7fffab531fa44ae9491331e06a5bcb8069b4048
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-07 02:03:59 +0100

    this days i rarely rest 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,48 +1,50 @@
 #include <stdio.h>
 /**
- * print_pair - a unique pair of two-digit numbers in ascending order.
- * @a: The first digit of the pair.
- * @b: The second digit of the pair.
- * @c: The third digit of the pair.
- * @d: The fourth digit of the pair.
- */
-
-void print_pair(char a, char b, char c, char d)
-{
-	putchar(a);
-	putchar(b);
-	putchar(' ');
-	putchar(c);
-	putchar(d);
-
-	if (a != '9' || b != '8' || c != '9' || d != '9')
-	{
-		putchar(',');
-		putchar(' ');
-	}
-}
-
-/**
- * main - Print unique pairs of two-digit numbers in ascending order
+ * main - main block
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01.
+ * Combination of numbers must be separated by a comma followed by a space.
+ * Combinations of numbers should be printed in ascending order.
+ * `00 01` and `01 00` are considered as the same combination.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 8 times.
+ * You are not allowed to use any variable of type `char`.
  * Return: 0
  */
 int main(void)
 {
-	char a, b, c, d;
+	int i, j;
+	int a, b, c, d;
 
-	for (a = '0'; a <= '9'; a++)
+	for (i = 0; i < 100; i++)
 	{
-		for (b = a; b <= '9'; b++)
-		{
-			for (c = '0'; c <= '9'; c++)
-			{
-				for (d = (a == b) ? (b + 1) : '0'; d <= '9'; d++)
-					print_pair(a, b, c, d);
+		a = i / 10; /* doubles fnum */
+		b = i % 10; /* singles fnum */
 
+		for (j = 0; j < 100; j++)
+		{
+			c = j / 10; /* doubles snum */
+			d = j % 10; /* singles snum */
+
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
 			}
 		}
 	}
+	putchar(10);
 
-	putchar('\n');
 	return (0);
 }
