097c0b7fc1f777ba07242557995ae3785f0c51bb
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-11 00:23:01 +0100

    fixes bugs in 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,9 +11,9 @@ void times_table(void)
 	int i, j;
 	int a, b, c;
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 11; j++)
+		for (j = 0; j < 10; j++)
 		{
 			a = i * j;
 			b = a / 10;
