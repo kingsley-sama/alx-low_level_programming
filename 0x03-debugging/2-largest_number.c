614bd9e7c92050f5ebc8312d3df659241b6bdde2
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-11 16:22:23 +0100

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,24 +10,15 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+	int largest = a;
 
-	if (a > b && b > c)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
+	if (b > a)
 	{
 		largest = b;
 	}
-	else if (a > c && c > b)
+	else if (a > c)
 	{
 		largest = a;
 	}
-	else
-	{
-		largest = c;
-	}
-
 	return (largest);
 }
