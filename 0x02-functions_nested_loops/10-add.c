b6ed27361780375c45afa092872042a134d3e493
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-11 07:22:19 +0100

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -11,21 +11,6 @@ int add(int a, int b)
 {
 
 	int sum = a + b;
-	int c = sum / 10;
-	int d = sum % 10;
 
-	if (sum >= 0)
-	{
-		c = sum / 10;
-		d = sum % 10;
-
-	}
-	else if (sum < 0)
-	{
-		c = sum / 10;
-		d = -1 * (sum % 10);
-
-		_putchar('-');
-	}
-	return sum;
+	return (sum);
 }
