3a47d2cba5ccafa42e6c6635e40d39e31a79a1b6
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-12 15:21:35 +0100

    fixes loopholes in 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,14 +9,18 @@ int main(void)
 {
 	char *fizz = "Fizz";
 	char *buzz = "Buzz";
+	char *fizzbuss = "FizzBuzz";
 	int i = 1;
 
 	while (i < 101)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 			printf("%s", fizz);
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 			printf("%s", buzz);
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("%s", fizzbuss);
+
 		else
 			printf("%d", 8);
 		i++;
