ec662a06664b46306aa04b01325ecbef47df0733
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-12 13:50:22 +0100

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - prints a square to the terminal;
+ *@size: the number of ‘#’ to be used
+ * Return: void
+ */
+void print_square(int size)
+{
+	int j = 0;
+
+	while (j < size)
+	{
+		int i;
+
+		while (i < size)
+		{
+			_putchar('#');
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
