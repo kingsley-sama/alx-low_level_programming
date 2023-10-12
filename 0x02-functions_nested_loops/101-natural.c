c1807044b2a95da5c824c554bff1d80fea844e43
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-12 01:46:43 +0100

    104-fibonacci.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 	int i, sum;
-	
 	sum = 0;
 	for (i = 0; i <= 1024; i++)
 	{
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,38 @@
-#include "main.h"
 #include <stdio.h>
+#define LARGEST 10000000000
 /**
- * fibonacc - this function checks if its parameter is lower case or uper case
- * @c: the parameter in form of an int
- * Return: 1 if lowercase and 0 if something else
+ * main - main block
+ * Description: Find and print the first 98 fib numbers starting with 1 and 2.
+ * Numbers should be coma and space separated.
+ * Return: 0
  */
-
 int main(void)
 {
-	int i = 0;
-	int a = 1;
-	int b = 2;
-	int sum;
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
 
-	_putchar(a + '0');
-	_putchar(b + '0');
-	sum = a + b;
-
-	int fib = sum
-	while (i < 96)
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
 	{
-		
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+		if (count != 97)
+			printf(", ");
 	}
+	printf("\n");
+	return (0);
 }
