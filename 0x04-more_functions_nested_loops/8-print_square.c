608e5b7b0e2da78edce22aa0948733851702a322
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-13 22:51:04 +0100

    3-print_remaining_days.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,7 +10,7 @@ void print_square(int size)
 {
 	int j;
 
-	if (size == 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
@@ -26,6 +26,5 @@ void print_square(int size)
 			}
 			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
