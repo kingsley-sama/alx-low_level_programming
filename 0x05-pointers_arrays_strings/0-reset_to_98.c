8cabed0fdb7754e9dd751ec760a6e211fad3c4a0
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-16 12:43:31 +0100

    initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - takes an integer poniter and changes the value at the address
+ *98
+ * Return: void
+ *@n: this is the pointer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#ifndef HEADER_FILES
+#define HEADER_FILES
+void reset_to_98(int *n);
+#endif
