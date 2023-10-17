02ccb0a064f945f8738e9e76d2e0ba75b0716aa6
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-17 03:22:16 +0100

    fixes bug in 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", *(a + i));
+		printf("%d, ", a[i]);
 	}
-
+	printf("%d", a[i + 1]);
 	putchar('\n');
 
 }
