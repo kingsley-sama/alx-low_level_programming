7997c15acee3a73ef5373d384f667c760edce6d6
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-19 14:46:55 +0100

    fixes bugs in 3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while (*(s1 + i) != '\0'  || *(s2 + i) != '\0')
+	for (; *(s1 + i) != '\0'  || *(s2 + i) != '\0'; i++)
 	{
 		if (*(s1 + i) != *(s2 + i))
 		{
@@ -21,7 +21,7 @@ int _strcmp(char *s1, char *s2)
 			else if (*(s1 + i) < *(s2 + i))
 				return (*(s1 + i) - *(s2 - i));
 		}
-		i++;
+
 
 	}
 	return (0);
