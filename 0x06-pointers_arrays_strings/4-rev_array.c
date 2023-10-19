14a2b377819438143caf824ee8a34ae57d29f42c
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-19 16:06:00 +0100

    testing 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -16,9 +16,9 @@ void reverse_array(int *a, int n)
 	{
 		int j;
 
-		j = a[i];
-		a[i] = a[n - 1 - i];
-		a[n - 2 - i] = j;
+		j = *(a + i);
+		*(a + i) = *(a + (n - 1 - i));
+		*(a + (n - 1 - i)) = j;
 		i++;
 	}
 
