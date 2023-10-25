45b6566477627ce35985ed28c8f20ddc15fefd8b
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-25 19:00:21 +0100

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_puts_recursion - This function
+ * Return: void
+ *@s: this i
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,4 @@
+#ifndef HEADER_FILES
+#define HEADER_FILES
+void _puts_recursion(char *s);
+#endif
