0bab27f32ff0b99467cb06aa3458d874cf077326
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-26 00:31:21 +0100

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,23 +1,35 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 /**
  *is_palindrome - This function
  * Return: void
  *@s: this i
+ *@start : this is the start index
+ *@end : this is the end index
  */
+int is_palindrome_recursive(char *s, int start, int end);
 int is_palindrome(char *s)
 {
-	int i, j;
+	int length = strlen(s);
 
-	i = 0;
-	j = strlen(s) - 1;
-	while (s[i] != '\0')
-	{
-		if (s[i] != s[j])
-			return (0);
-		i++;
-		j--;
-	}
-	return (1);
+	if (length <= 1)
+		return (1);
+	return (is_palindrome_recursive(s, 0, length - 1));
+}
+/**
+ *is_palindrome_recursive - This function
+ *@s: this is the string input
+ *@start : this is the start index
+ *@end : this is the end index
+ * Return: 1 if succes 0 if failure
+ */
+int is_palindrome_recursive(char *s, int start, int end)
+{
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (is_palindrome_recursive(s, start + 1, end - 1));
 }
