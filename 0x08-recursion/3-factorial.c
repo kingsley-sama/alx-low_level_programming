26c781aa3284668d24981a98854bdf78e264a872
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-26 01:33:25 +0100

    4-pow_recursion.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,16 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *_strlen_recursion - This returns the lenght of a string
- * Return: void
- *@s: string input
- */
+ *factorial - prints the factorial of a number
+ * Return: n factorial
+ *@n: the integer input
+*/
 int factorial(int n)
 {
 	if (n == 0)
 		return (1);
 	if (n < 0)
 		return (-1);
-	n = n * factorial(n-1);
+	n = n * factorial(n - 1);
 	return (n);
 }
