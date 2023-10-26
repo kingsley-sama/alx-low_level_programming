10a1678a50e28aaa4e279d99e48ed9483c25e6ec
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-26 01:18:27 +0100

    2-strlen_recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *_strlen_recursion - This returns the lenght of a string
+ *_print_rev_recursion - This function reverses a string
  * Return: void
- *@s: string input
+ *@s: this is the string
  */
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-
 	if (*s == '\0')
 	{
-		return(1);
+		putchar('\n');
+		return;
 	}
-	return(_strlen_recursion(s + 1) + 1);
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+
 }
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *_puts_recursion - This function
+ *_strlen_recursion - This returns the lenght of a string
  * Return: void
- *@s: this i
+ *@s: string input
  */
-void _puts_recursion(char *s)
+int _strlen_recursion(char *s)
 {
+
 	if (*s == '\0')
 	{
-		putchar('\n');
-		return;
+		return (1);
 	}
-	_puts_recursion(s + 1);
-	putchar(*s);
-
+	return (_strlen_recursion(s + 1) + 1);
 }
