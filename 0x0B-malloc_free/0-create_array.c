8986d7e4de5d30ed1eefa34d5e78a73aa5a70869
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-30 18:50:55 +0100

    fixed errors in 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,16 +12,14 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *arr = (char *) malloc(size * sizeof(char));
 
-	if (size > 0)
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
 	{
 		for (i = 0; i < size; i++)
-		{
 			*(arr + i) = c;
-
-		}
 		return (arr);
 	}
-	return (arr);
-
-
 }
