3cb76a7bdc3bbfe7ef299a7677ea7d9081b21f1e
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-30 19:06:47 +0100

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ *_strdup - creates a duplicate of it's string input
+ *@str: string input
+ *Return: a pointer to the duplicate
+ */
+char *_strdup(char *str)
+{
+	unsigned int i, size;
+	char *arr = (char *) malloc(sizeof(char) * strlen(str));
+
+	size = strlen(str);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+			arr[i] = str[i];
+		return (arr);
+	}
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,4 +3,5 @@
 #include <stdio.h>
 #include <string.h>
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 #endif
