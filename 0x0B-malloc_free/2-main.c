b888d317b3b1c4964ce7329c592fe070285928e3
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-31 16:21:57 +0100

    fixes bugs in 2-str_concat.c

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -11,12 +11,9 @@ int main(void)
 {
 	char *s;
 
-	/* s = str_concat("Betty ", "Holberton"); */
-	s= str_concat("Hello", NULL);
-	/* s = str_concat(NULL, "Hello"); */
+	s = str_concat("Betty ", "Holberton"); 
 	/* s =    str_concat(NULL, NULL); */
 	/* s =  str_concat("Hello", ""); */
-	/* s =    str_concat("", "Hello"); */
 	if (s == NULL)
 	{
 		printf("failed\n");
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,45 +13,19 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k;
 	int a = 0, b = 0;
 	char *arr;
-	char *str1, *str2;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (; s1[i] != '\0'; i++)
+		;
+	for (; s2[j] != '\0'; j++)
+		;
+	k = i + j;
+	arr = (char *)malloc(sizeof(char) * k);
+	if (arr != NULL)
 	{
-		str1 = "";
-		return (str1);
-	}
-	else if (s2 == NULL && s1 != NULL)
-	{
-		str1 = (char *)malloc(sizeof(char) * strlen(s1) + 1);
-		if (str1 != NULL)
-		{
-			for (; s1[i] != '\0'; i++)
-				str1[i] = s1[i];
-			str1[i + 1] = '\0';
-			return (str1);
-		}
-		return (NULL);
-	}
-	else if (s1 == NULL && s2 != NULL)
-	{
-		str2 = (char *)malloc(sizeof(char) * strlen(s2) + 1);
-		if (str2 != NULL)
-		{
-			for (; s2[i] != '\0'; i++)
-				str2[i] = s2[i];
-			str2[i + 1] = '\0';
-			return (str2);
-		}
-		return (NULL);
-	}
-	else
-	{
-		for (; s1[i] != '\0'; i++)
-			;
-		for (; s1[j] != '\0'; j++)
-			;
-		k = i + j + 3;
-		arr = (char *)malloc(sizeof(char) * k);
 		for (; s1[a] != '\0'; a++)
 			arr[a] = s1[a];
 		for (; s2[b] != '\0'; b++)
@@ -59,5 +33,5 @@ char *str_concat(char *s1, char *s2)
 		arr[k] = '\0';
 		return (arr);
 	}
+	return (NULL);
 }
-
