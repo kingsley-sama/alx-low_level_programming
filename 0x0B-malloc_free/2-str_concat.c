5f8746925e9585ffd12c4b6ad9c410aab70fb035
Kingsley Okpo <kngsley2018@gmail.com>
2023-10-31 15:24:52 +0100

    2-str_concat.c

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *str_concat - concatenates to strings
+ *@s1: first input
+ *@s2: second input
+ *Return: a pointer to the duplicate
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, k;
+	int a = 0, b = 0;
+	char *arr;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		return ("");
+	}
+	else if (s2 == NULL)
+	{
+		return (s1);
+	}
+	else if (s1 == NULL)
+	{
+		return (s2);
+	}
+	else
+	{
+		for (; s1[i] != '\0'; i++)
+			;
+		for (; s1[j] != '\0'; j++)
+			;
+		k = i + j + 3;
+		arr = (char *)malloc(sizeof(char) * k);
+		for (; s1[a] != '\0'; a++)
+			arr[a] = s1[a];
+		for (; s2[b] != '\0'; b++)
+			arr[a + b] = s2[b];
+		arr[k] = '\0';
+		return (arr);
+	}
+}
+
