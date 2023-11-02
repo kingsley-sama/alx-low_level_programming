273193fdcab7e3d0bb6606c2f49d9de20f99c80e
Kingsley Okpo <kngsley2018@gmail.com>
2023-11-02 18:30:09 +0100

    fixes bugs in 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	arr = calloc(sizeof(int), range);
+	arr = calloc(sizeof(int), range + 1);
 	if (arr == NULL)
 		return (NULL);
 	while (i <= range)
