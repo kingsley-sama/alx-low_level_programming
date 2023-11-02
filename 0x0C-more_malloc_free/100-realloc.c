c269e0a5d00d0ed7aca7655e35ef7e92d9b6c13e
Kingsley Okpo <kngsley2018@gmail.com>
2023-11-02 19:23:50 +0100

    100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *_realloc - this function reinitializes a memory space
+ *@ptr: this is the pointer to the previous address
+ *@old_size: this is the size of the former pointer
+ *@new_size: this is the size of the new pointer
+ *Return: void pointer to the address
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *arr;
+
+	if (new_size > old_size)
+	{
+		arr = realloc(ptr, new_size);
+		return (arr);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		arr = malloc(new_size);
+		return (arr);
+	}
+	else if (ptr == NULL)
+	{
+		arr = malloc(new_size);
+		return (arr);
+	}
+	if (new_size == 0 && ptr != NULL)
+		free(ptr);
+	arr = realloc(ptr, new_size);
+	if (arr == NULL)
+		return (NULL);
+	return (arr);
+}
