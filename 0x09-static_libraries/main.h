822c65bc66bec473706145f7dcf8f9d4e41c6fd2
Kingsley Okpo <kngsley2018@gmail.com>
2023-11-02 01:20:01 +0100

    fixes persitent errors in libmy.a

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,5 @@
+#ifndef LIBMY_A
+#define LIBMY_A
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
