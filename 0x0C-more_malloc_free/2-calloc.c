3f59d75cd8800f234916790151ba630d35736621
Kingsley Okpo <kngsley2018@gmail.com>
2023-11-02 16:59:34 +0100

    fixes error in 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(nmemb * size + 1);
+	ptr = calloc(nmemb, size);
 	if (ptr == NULL)
 		return (NULL);
 	return (ptr);
