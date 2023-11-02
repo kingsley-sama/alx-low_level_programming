3222c86a355463d3b862c02bb502867651de5390
Kingsley Okpo <kngsley2018@gmail.com>
2023-11-02 05:11:31 +0100

    100-change.c

--- 0x0A-argc_argv/100-change_alt.c
+++ 0x0A-argc_argv/100-change_alt.c
@@ -10,22 +10,26 @@
 int main(int argc, char **argv)
 {
 	int cents[5] = {25, 10, 5, 2, 1};
-	int amount;
-	int i = 0, j = 0;
+	int total = 0;
+	int amount = atoi(argv[1]);
+	int i = 0,j = 0;
+	
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	amount = atoi(argv[1]);
-	if (amount < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-	for (i = 0; i < 5; i++)
-	{
+	/* 	while (i < 5) */
+	/* 	{ */
+	/* 		while (total <= amount) */
+	/* 		{ */
+	/* 			if (cents[j] > amount) */
+	/* 				break; */
+	/* 			total += cents[j]; */
+	/* 		} */
+	/* 		j--; */
+	/* 		i++; */
+	/* 	} */
+	/* 	printf("%d\n", j); */
+	/* 	return (argc); */
+	/* } */
+	for (i = 0; i < 5; i++) {
 		while (amount >= cents[i])
 		{
 			amount -= cents[i];
