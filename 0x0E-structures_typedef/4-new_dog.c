995d2551d88d04ca7999fb1627de324ca5fb1bc3
Kingsley Okpo <kngsley2018@gmail.com>
2023-11-06 22:56:11 +0100

    fixes bugs in 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,12 +14,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t my_dog;
 	dog_t *p = malloc(sizeof(my_dog));
-	
+
 	if (p)
 	{
+		p->name = strdup(name);
+		p->owner = strdup(owner);
+		if (p->name == NULL || p->owner == NULL)
+		{
+			free(p->owner);
+			free(p->name);
+			return (NULL);
+		}
 		p->age = age;
-		p->name = strcpy(p->name, name);
-		p->owner = strcpy(p->owner, owner);
 		return (p);
 	}
 	return (NULL);
