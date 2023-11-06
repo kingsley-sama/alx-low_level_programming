0dfd8e7214f81e6d63c83938768a7b279794df6c
Kingsley Okpo <kngsley2018@gmail.com>
2023-11-06 23:31:34 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,28 +4,32 @@
 #include "dog.h"
 
 /**
- * new_dog - creates a new instance of the dog struc
+ * new_dog - creates a new instance of the dog struct
  * @name: the name of the dog instance
  * @age: the age of the dog instance
  * @owner: the owner of the dog instance
- * Return: 0 if success , NULL if failure
+ * Return: a pointer to the new dog instance, or NULL if failure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t my_dog;
-	dog_t *p = malloc(sizeof(my_dog));
+	dog_t *p = malloc(sizeof(dog_t));
 
 	if (p != NULL)
 	{
 		p->name = strdup(name);
 		p->owner = strdup(owner);
+
 		if (p->name == NULL || p->owner == NULL)
 		{
-			free(p->owner);
 			free(p->name);
+			free(p->owner);
+			free(p);
+			return (NULL);
 		}
+
 		p->age = age;
 		return (p);
 	}
+
 	return (NULL);
 }
