1ac2f792df318d74e22198e84a4da6b73c7e99de
Kingsley Okpo <kngsley2018@gmail.com>
2023-11-06 23:14:29 +0100

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,11 +9,12 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Name: %s\n", d->name ? d->name : "(nil)\n");
 		if (d->age)
 			printf("Age: %f\n", d->age);
 		else
 			printf("Age: (nil)\n");
 		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
+	
 }
