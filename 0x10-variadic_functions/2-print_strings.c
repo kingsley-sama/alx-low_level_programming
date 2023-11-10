ad1777acd72ccaac028cfec733dc871a65467116
Kingsley Okpo <kngsley2018@gmail.com>
2023-11-10 04:56:52 +0100

    1-print_numbers.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nill)");
 		else
 			printf("%s", str);
-		if (i != n - 1)
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 		i++;
 	}
