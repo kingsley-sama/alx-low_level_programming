43075b8037e05456e5da9505c0fad236823a8352
Kingsley Okpo <kngsley2018@gmail.com>
2023-11-16 12:25:36 +0100

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,45 +1,19 @@
-1	.file	"101-hello_holberton.c"
-	.text
-	.section	.rodata
-.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.globl	main
-	.type	main, @function
+section .data
+    hello_message db "Hello, Holberton", 0
+
+section .text
+    global main
+    extern printf
+
 main:
-.LFB0:
-	.cfi_startproc
-	endbr64
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	leaq	.LC0(%rip), %rax
-	movq	%rax, %rdi
-	call	puts@PLT
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	1f - 0f
-	.long	4f - 1f
-	.long	5
-0:
-	.string	"GNU"
-1:
-	.align 8
-	.long	0xc0000002
-	.long	3f - 2f
-2:
-	.long	0x3
-3:
-	.align 8
-4:
+    ; Prepare arguments for printf
+    mov rdi, hello_message ; Format string
+    mov rax, 0             ; Number of vector registers used (variadic function)
+    call printf            ; Call printf function
+
+    ; Exit the program
+    mov rax, 60            ; syscall: exit
+    xor rdi, rdi           ; status: 0
+    syscall
+
+section .data
