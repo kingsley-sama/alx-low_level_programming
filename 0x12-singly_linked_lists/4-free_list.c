a50b0397135c479d2e58d61c18a32b27dd94d0a1
Kingsley Okpo <kngsley2018@gmail.com>
2023-11-17 00:57:26 +0100

    fixes errors in 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,14 +7,14 @@
  */
 void free_list(list_t *head)
 {
-    list_t *current = head;
-    list_t *next_node;
+	list_t *current = head;
+	list_t *next_node;
 
-    while (current != NULL)
-    {
-        next_node = current->next;
-        free(current->str);
-        free(current);
-        current = next_node;
-    }
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current->str);
+		free(current);
+		current = next_node;
+	}
 }
