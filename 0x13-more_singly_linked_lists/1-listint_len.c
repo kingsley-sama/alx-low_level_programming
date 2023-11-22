27716c2a23ac68bb515724c59395b6aef7d83227
Kingsley Okpo <kngsley2018@gmail.com>
2023-11-22 19:05:02 +0100

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ *listint_len - returns length of a linked list
+ *@h: pointer to a singly linked list
+ *Return: the length of the linked list
+ *
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+	const listint_t *copy = h;
+
+	if (copy == NULL)
+		return (0);
+	while (copy != NULL)
+	{
+		i++;
+	}
+	return (i);
+}
