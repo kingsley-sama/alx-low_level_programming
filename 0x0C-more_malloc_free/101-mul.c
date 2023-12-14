6343769eae5784a14d8ce111ba2726187cffb03c
Kingsley Okpo <kngsley2018@gmail.com>
2023-12-14 15:59:37 +0100

    0x14-bit_manipulation/

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  *main - multiplies arguments passed to it
  *@argc: total number of command line arguments
@@ -9,13 +10,16 @@
  */
 int main(int argc, char **argv)
 {
+	char *arg1, *arg2;
 	long int a, b;
-
+	
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	arg1 = malloc(strlen(*(argv + 1)) * sizeof(char));
+	arg2 = malloc(strlen(*(argv + 2)) * sizeof(char));
 	a = atoi(*(argv + 1));
 	b = atoi(*(argv + 2));
 	printf("%ld\n", a * b);
--- 0x13-more_singsley_linked_lists/README.md
+++
@@ -1,3 +0,0 @@
-# 0x13-more_singsley_linked_lists
-## 0-print_listint
-- prints the items in a singly linked list
